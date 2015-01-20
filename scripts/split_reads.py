785025806ea034e1d260941ec04e588b97b3f385
Nick Booher <njbooher@gmail.com>
2015-01-19 21:08:52 -0500

    Remove special characters from sequence filenames

--- scripts/split_reads.py
+++ scripts/split_reads.py
@@ -1,4 +1,4 @@
 from Bio import SeqIO
 import sys
 for entry in SeqIO.parse(sys.argv[1], "fasta"):
-    SeqIO.write([entry], sys.argv[2] + "/" + entry.id.replace("/", "_") + ".fasta", "fasta")
+    SeqIO.write([entry], sys.argv[2] + "/" + entry.id.replace("/", "_").replace("|", "_").replace(".", "_") + ".fasta", "fasta")

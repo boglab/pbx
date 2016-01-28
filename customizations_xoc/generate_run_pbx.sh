ccf43453bc57c9a84c430f8e53ced9747519628c
Nick Booher <njbooher@gmail.com>
2016-01-28 10:50:43 -0600

    Resequence Canu assembly

--- customizations_xoc/generate_run_pbx.sh
+++ customizations_xoc/generate_run_pbx.sh
@@ -4,12 +4,16 @@ if [ -f /opt/pbx/run_pbx.sh ]; then
     mv /opt/pbx/run_pbx.sh /opt/pbx/run_pbx_bak.sh
 fi
 
-if [ -f /opt/pbx/run_pbx_hgap.sh ]; then
-    mv /opt/pbx/run_pbx_hgap.sh /opt/pbx/run_pbx_hgap_bak.sh
+if [ -f /opt/pbx/run_pbx_pre_smrtenv.sh ]; then
+    mv /opt/pbx/run_pbx_pre_smrtenv.sh /opt/pbx/run_pbx_pre_smrtenv_bak.sh
 fi
 
-if [ -f /opt/pbx/run_pbx_post_hgap.sh ]; then
-    mv /opt/pbx/run_pbx_post_hgap.sh /opt/pbx/run_pbx_post_hgap_bak.sh
+if [ -f /opt/pbx/run_pbx_smrtenv.sh ]; then
+    mv /opt/pbx/run_pbx_smrtenv.sh /opt/pbx/run_pbx_smrtenv_bak.sh
+fi
+
+if [ -f /opt/pbx/run_pbx_post_smrtenv.sh ]; then
+    mv /opt/pbx/run_pbx_post_smrtenv.sh /opt/pbx/run_pbx_post_smrtenv_bak.sh
 fi
 
 #rm -f /opt/pbx/run_pbx.sh
@@ -33,6 +37,11 @@ for rawfull in /opt/raw_long_*; do
     mkdir -p ${f}/canu_assembly
     mkdir -p ${f}/canu_assembly/dotplots
     mkdir -p ${f}/canu_assembly/exported
+    
+    mkdir -p ${f}/canu_assembly/resequencing
+    find ${rawfull}/ -name "*.bax.h5" | sort > ${f}/canu_assembly/resequencing/input.fofn
+    cp /opt/pbx/customizations_xoc/RS_Resequencing_HGAP_Xo.1.xml ${f}/canu_assembly/resequencing/settings.xml
+    sed -i "s/HGAP_resequencing/ref_${fprefix}_Canu/g" ${f}/canu_assembly/resequencing/settings.xml
     
     mkdir -p ${f}/hgap2_assembly
     mkdir -p ${f}/hgap2_assembly/dotplots
@@ -62,58 +71,65 @@ for rawfull in /opt/raw_long_*; do
     
 done
 
-echo "/opt/pbx/run_pbx_hgap.sh" >> /opt/pbx/run_pbx.sh
-echo "/opt/pbx/run_pbx_post_hgap.sh" >> /opt/pbx/run_pbx.sh
+echo "/opt/pbx/run_pbx_pre_smrtenv.sh" >> /opt/pbx/run_pbx.sh
+echo "/opt/pbx/run_pbx_smrtenv.sh" >> /opt/pbx/run_pbx.sh
+echo "/opt/pbx/run_pbx_post_smrtenv.sh" >> /opt/pbx/run_pbx.sh
 
-echo "source /opt/smrtanalysis/current/etc/setup.sh" >> /opt/pbx/run_pbx_hgap.sh
+echo "source /opt/smrtanalysis/current/etc/setup.sh" >> /opt/pbx/run_pbx_smrtenv.sh
 
 for rawfull in /opt/raw_long_*; do
     
     fprefix=`echo "${rawfull}" | cut -c 15-`
     f="/opt/pbx/results/${fprefix}"
     
-    echo "fofnToSmrtpipeInput.py ${f}/hgap2_assembly/input.fofn --jobname='HGAP2_Assembly_Job' > ${f}/hgap2_assembly/input.xml" >> /opt/pbx/run_pbx_hgap.sh
-    echo "smrtpipe.py --params=${f}/hgap2_assembly/settings.xml --output=${f}/hgap2_assembly/ xml:${f}/hgap2_assembly/input.xml > ${f}/logs/hgap2_assembly.txt 2>&1" >> /opt/pbx/run_pbx_hgap.sh
-    echo "gunzip -c ${f}/hgap2_assembly/data/polished_assembly.fasta.gz > ${f}/hgap2_assembly/data/polished_assembly.fasta" >> /opt/pbx/run_pbx_hgap.sh
+    echo "bash -e /opt/pbx/customizations_xoc/run_canu/run_me.sh ${f} ${fprefix} ${rawfull} > ${f}/logs/canu_assembly.txt 2>&1" >> /opt/pbx/run_pbx_pre_smrtenv.sh
+    echo "referenceUploader  --samIdx='samtools faidx' --saw='sawriter -blt 8 -welter' -c -n ref_${fprefix}_Canu -f ${f}/canu_assembly/canu-output/${fprefix}.consensus.fasta" >> /opt/pbx/run_pbx_smrtenv.sh
+    echo "cp -R /opt/smrtanalysis/current/common/references/ref_${fprefix}_Canu/ ${f}/canu_assembly/resequencing/"
+    echo "fofnToSmrtpipeInput.py ${f}/canu_assembly/resequencing/input.fofn --jobname='Canu_Resequencing_Job' > ${f}/canu_assembly/resequencing/input.xml" >> /opt/pbx/run_pbx_smrtenv.sh
+    echo "smrtpipe.py --params=${f}/canu_assembly/resequencing/settings.xml --output=${f}/canu_assembly/resequencing/ xml:${f}/canu_assembly/resequencing/input.xml > ${f}/logs/canu_resequencing.txt 2>&1" >> /opt/pbx/run_pbx_smrtenv.sh
+    echo "gunzip -c ${f}/canu_assembly/resequencing/data/consensus.fasta.gz > ${f}/canu_assembly/resequencing/data/consensus.fasta" >> /opt/pbx/run_pbx_smrtenv.sh
     
-    echo "fofnToSmrtpipeInput.py ${f}/hgap3_assembly/input.fofn --jobname='HGAP3_Assembly_Job' > ${f}/hgap3_assembly/input.xml" >> /opt/pbx/run_pbx_hgap.sh
-    echo "smrtpipe.py --params=${f}/hgap3_assembly/settings.xml --output=${f}/hgap3_assembly/ xml:${f}/hgap3_assembly/input.xml > ${f}/logs/hgap3_assembly.txt 2>&1" >> /opt/pbx/run_pbx_hgap.sh
-    echo "gunzip -c ${f}/hgap3_assembly/data/polished_assembly.fasta.gz > ${f}/hgap3_assembly/data/polished_assembly.fasta" >> /opt/pbx/run_pbx_hgap.sh
+    echo "fofnToSmrtpipeInput.py ${f}/hgap2_assembly/input.fofn --jobname='HGAP2_Assembly_Job' > ${f}/hgap2_assembly/input.xml" >> /opt/pbx/run_pbx_smrtenv.sh
+    echo "smrtpipe.py --params=${f}/hgap2_assembly/settings.xml --output=${f}/hgap2_assembly/ xml:${f}/hgap2_assembly/input.xml > ${f}/logs/hgap2_assembly.txt 2>&1" >> /opt/pbx/run_pbx_smrtenv.sh
+    echo "gunzip -c ${f}/hgap2_assembly/data/polished_assembly.fasta.gz > ${f}/hgap2_assembly/data/polished_assembly.fasta" >> /opt/pbx/run_pbx_smrtenv.sh
+    
+    echo "fofnToSmrtpipeInput.py ${f}/hgap3_assembly/input.fofn --jobname='HGAP3_Assembly_Job' > ${f}/hgap3_assembly/input.xml" >> /opt/pbx/run_pbx_smrtenv.sh
+    echo "smrtpipe.py --params=${f}/hgap3_assembly/settings.xml --output=${f}/hgap3_assembly/ xml:${f}/hgap3_assembly/input.xml > ${f}/logs/hgap3_assembly.txt 2>&1" >> /opt/pbx/run_pbx_smrtenv.sh
+    echo "gunzip -c ${f}/hgap3_assembly/data/polished_assembly.fasta.gz > ${f}/hgap3_assembly/data/polished_assembly.fasta" >> /opt/pbx/run_pbx_smrtenv.sh
 
-    #echo "referenceUploader  --samIdx='samtools faidx' --saw='sawriter -blt 8 -welter' -c -n ref_${fprefix}_HGAP2 -f ${f}/hgap2_assembly/data/polished_assembly.fasta" >> /opt/pbx/run_pbx_hgap.sh
+    #echo "referenceUploader  --samIdx='samtools faidx' --saw='sawriter -blt 8 -welter' -c -n ref_${fprefix}_HGAP2 -f ${f}/hgap2_assembly/data/polished_assembly.fasta" >> /opt/pbx/run_pbx_smrtenv.sh
     #echo "cp -R /opt/smrtanalysis/current/common/references/ref_${fprefix}_HGAP2/ ${f}/hgap2_resequencing/"
-    #echo "fofnToSmrtpipeInput.py ${f}/hgap2_resequencing/input.fofn --jobname='HGAP2_Resequencing_Job' > ${f}/hgap2_resequencing/input.xml" >> /opt/pbx/run_pbx_hgap.sh
-    #echo "smrtpipe.py --params=${f}/hgap2_resequencing/settings.xml --output=${f}/hgap2_resequencing/ xml:${f}/hgap2_resequencing/input.xml > ${f}/logs/hgap2_resequencing.txt 2>&1" >> /opt/pbx/run_pbx_hgap.sh
+    #echo "fofnToSmrtpipeInput.py ${f}/hgap2_resequencing/input.fofn --jobname='HGAP2_Resequencing_Job' > ${f}/hgap2_resequencing/input.xml" >> /opt/pbx/run_pbx_smrtenv.sh
+    #echo "smrtpipe.py --params=${f}/hgap2_resequencing/settings.xml --output=${f}/hgap2_resequencing/ xml:${f}/hgap2_resequencing/input.xml > ${f}/logs/hgap2_resequencing.txt 2>&1" >> /opt/pbx/run_pbx_smrtenv.sh
 
-    #echo "referenceUploader  --samIdx='samtools faidx' --saw='sawriter -blt 8 -welter' -c -n ref_${fprefix}_HGAP3 -f ${f}/hgap3_assembly/data/polished_assembly.fasta" >> /opt/pbx/run_pbx_hgap.sh
+    #echo "referenceUploader  --samIdx='samtools faidx' --saw='sawriter -blt 8 -welter' -c -n ref_${fprefix}_HGAP3 -f ${f}/hgap3_assembly/data/polished_assembly.fasta" >> /opt/pbx/run_pbx_smrtenv.sh
     #echo "cp -R /opt/smrtanalysis/current/common/references/ref_${fprefix}_HGAP3/ ${f}/hgap3_resequencing/"
-    #echo "fofnToSmrtpipeInput.py ${f}/hgap3_resequencing/input.fofn --jobname='HGAP3_Resequencing_Job' > ${f}/hgap3_resequencing/input.xml" >> /opt/pbx/run_pbx_hgap.sh
-    #echo "smrtpipe.py --params=${f}/hgap3_resequencing/settings.xml --output=${f}/hgap3_resequencing/ xml:${f}/hgap3_resequencing/input.xml > ${f}/logs/hgap3_resequencing.txt 2>&1" >> /opt/pbx/run_pbx_hgap.sh
-
-    echo "bash -e /opt/pbx/customizations_xoc/run_canu/run_me.sh ${f} ${fprefix} ${rawfull} > ${f}/logs/canu_assembly.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
+    #echo "fofnToSmrtpipeInput.py ${f}/hgap3_resequencing/input.fofn --jobname='HGAP3_Resequencing_Job' > ${f}/hgap3_resequencing/input.xml" >> /opt/pbx/run_pbx_smrtenv.sh
+    #echo "smrtpipe.py --params=${f}/hgap3_resequencing/settings.xml --output=${f}/hgap3_resequencing/ xml:${f}/hgap3_resequencing/input.xml > ${f}/logs/hgap3_resequencing.txt 2>&1" >> /opt/pbx/run_pbx_smrtenv.sh
     
-    echo "bash -e /opt/pbx/customizations_xoc/run_tale_export_hgap.sh ${f}/hgap2_assembly/data/polished_assembly.fasta ${f}/hgap2_assembly/exported ${PATTERNSTRAIN} > ${f}/logs/hgap2_export.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
-    echo "bash -e /opt/pbx/customizations_xoc/generate_tiling_dotplots/self_similarity/run_me_nucmer.sh ${f}/hgap2_assembly/data/polished_assembly.fasta ${f}/hgap2_assembly/dotplots > ${f}/logs/hgap2_dotplots.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
-    echo "python2 /opt/pbx/customizations_xoc/run_overlapper_tests/CA_best_edge_to_GML.py ${f}/hgap2_assembly/data/celera-assembler.gkpStore ${f}/hgap2_assembly/data/celera-assembler.tigStore ${f}/hgap2_assembly/data/celera-assembler.gkpStore.fastqUIDmap ${f}/hgap2_assembly/data/4-unitigger/best.edges ${f}/hgap2_assembly/data/ca_best_edges.gml" >> /opt/pbx/run_pbx_hgap.sh
+    echo "bash -e /opt/pbx/customizations_xoc/run_tale_export_hgap.sh ${f}/hgap2_assembly/data/polished_assembly.fasta ${f}/hgap2_assembly/exported ${PATTERNSTRAIN} > ${f}/logs/hgap2_export.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
+    echo "bash -e /opt/pbx/customizations_xoc/generate_tiling_dotplots/self_similarity/run_me_nucmer.sh ${f}/hgap2_assembly/data/polished_assembly.fasta ${f}/hgap2_assembly/dotplots > ${f}/logs/hgap2_dotplots.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
+    echo "python2 /opt/pbx/customizations_xoc/run_overlapper_tests/CA_best_edge_to_GML.py ${f}/hgap2_assembly/data/celera-assembler.gkpStore ${f}/hgap2_assembly/data/celera-assembler.tigStore ${f}/hgap2_assembly/data/celera-assembler.gkpStore.fastqUIDmap ${f}/hgap2_assembly/data/4-unitigger/best.edges ${f}/hgap2_assembly/data/ca_best_edges.gml" >> /opt/pbx/run_pbx_smrtenv.sh
     
-    echo "bash -e /opt/pbx/customizations_xoc/run_tale_export_hgap.sh ${f}/hgap3_assembly/data/polished_assembly.fasta ${f}/hgap3_assembly/exported ${PATTERNSTRAIN} > ${f}/logs/hgap3_export.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
-    echo "bash -e /opt/pbx/customizations_xoc/generate_tiling_dotplots/self_similarity/run_me_nucmer.sh ${f}/hgap3_assembly/data/polished_assembly.fasta ${f}/hgap3_assembly/dotplots > ${f}/logs/hgap3_dotplots.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
-    echo "python2 /opt/pbx/customizations_xoc/run_overlapper_tests/CA_best_edge_to_GML.py ${f}/hgap3_assembly/data/celera-assembler.gkpStore ${f}/hgap3_assembly/data/celera-assembler.tigStore ${f}/hgap3_assembly/data/celera-assembler.gkpStore.fastqUIDmap ${f}/hgap3_assembly/data/4-unitigger/best.edges ${f}/hgap3_assembly/data/ca_best_edges.gml" >> /opt/pbx/run_pbx_hgap.sh
+    echo "bash -e /opt/pbx/customizations_xoc/run_tale_export_hgap.sh ${f}/hgap3_assembly/data/polished_assembly.fasta ${f}/hgap3_assembly/exported ${PATTERNSTRAIN} > ${f}/logs/hgap3_export.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
+    echo "bash -e /opt/pbx/customizations_xoc/generate_tiling_dotplots/self_similarity/run_me_nucmer.sh ${f}/hgap3_assembly/data/polished_assembly.fasta ${f}/hgap3_assembly/dotplots > ${f}/logs/hgap3_dotplots.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
+    echo "python2 /opt/pbx/customizations_xoc/run_overlapper_tests/CA_best_edge_to_GML.py ${f}/hgap3_assembly/data/celera-assembler.gkpStore ${f}/hgap3_assembly/data/celera-assembler.tigStore ${f}/hgap3_assembly/data/celera-assembler.gkpStore.fastqUIDmap ${f}/hgap3_assembly/data/4-unitigger/best.edges ${f}/hgap3_assembly/data/ca_best_edges.gml" >> /opt/pbx/run_pbx_smrtenv.sh
     
-    echo "bash -e /opt/pbx/customizations_xoc/run_tale_export_hgap.sh ${f}/canu_assembly/canu-output/${fprefix}.consensus.fasta ${f}/canu_assembly/exported ${PATTERNSTRAIN} > ${f}/logs/canu_export.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
-    echo "bash -e /opt/pbx/customizations_xoc/generate_tiling_dotplots/self_similarity/run_me_nucmer.sh ${f}/canu_assembly/canu-output/${fprefix}.consensus.fasta ${f}/canu_assembly/dotplots > ${f}/logs/canu_dotplots.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
+    echo "bash -e /opt/pbx/customizations_xoc/run_tale_export_hgap.sh ${f}/canu_assembly/resequencing/data/consensus.fasta ${f}/canu_assembly/exported ${PATTERNSTRAIN} > ${f}/logs/canu_export.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
+    echo "bash -e /opt/pbx/customizations_xoc/generate_tiling_dotplots/self_similarity/run_me_nucmer.sh ${f}/canu_assembly/resequencing/data/consensus.fasta ${f}/canu_assembly/dotplots > ${f}/logs/canu_dotplots.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
     
-    echo "bash -e /opt/pbx/customizations_xoc/generate_raw_tale_read_dotplots/run_me.sh ${f} > ${f}/logs/raw_tale_read_dotplots.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
+    echo "bash -e /opt/pbx/customizations_xoc/generate_raw_tale_read_dotplots/run_me.sh ${f} > ${f}/logs/raw_tale_read_dotplots.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
     
-    echo "bash -e /opt/pbx/customizations_xoc/run_overlapper_tests/run_me.sh ${f} > ${f}/logs/overlapper_tests.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
+    echo "bash -e /opt/pbx/customizations_xoc/run_overlapper_tests/run_me.sh ${f} > ${f}/logs/overlapper_tests.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
     
-    echo "bash -e /opt/pbx/customizations_xoc/generate_qc_report/run_me.sh ${f} > ${f}/qc_report.txt 2>&1" >> /opt/pbx/run_pbx_post_hgap.sh
+    echo "bash -e /opt/pbx/customizations_xoc/generate_qc_report/run_me.sh ${f} > ${f}/qc_report.txt 2>&1" >> /opt/pbx/run_pbx_post_smrtenv.sh
     
 done
 
 chmod +x /opt/pbx/run_pbx.sh
-chmod +x /opt/pbx/run_pbx_hgap.sh
-chmod +x /opt/pbx/run_pbx_post_hgap.sh
+chmod +x /opt/pbx/run_pbx_pre_smrtenv.sh
+chmod +x /opt/pbx/run_pbx_smrtenv.sh
+chmod +x /opt/pbx/run_pbx_post_smrtenv.sh
 
 for rawfull in /opt/raw_long_*; do
     

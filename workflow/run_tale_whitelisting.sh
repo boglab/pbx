b6181066790e2681640bc1de74682bfb4ebd3b83
Nick Booher <njbooher@gmail.com>
2016-12-23 06:29:51 -0600

    Exclude seed read example (#5)
    * Update run_tale_whitelisting.sh

--- workflow/run_tale_whitelisting.sh
+++ workflow/run_tale_whitelisting.sh
@@ -18,5 +18,5 @@ for f in *.bax.h5; do fprefix=`echo "$f" | rev | cut -c 10- | rev`; echo $fprefi
 
 cd ${RESULTS_PATH}
 for f in *_p0.align; do fprefix=`echo "$f" | rev | cut -c 7- | rev`; echo ${fprefix}; python ${PBX_SCRIPTS_PATH}/bash5_alignment_to_filter_lists.py ${fprefix}.align ${fprefix}-talreads.txt ${fprefix}-nontalreads.txt; done;
-cat *-talreads.txt > talreads.txt
+cat *-talreads.txt | grep -v 'm150916_071026_42177R_c100856882550000001823190901241611_s1_p0/141912/0_20842' > talreads.txt
 cat *-nontalreads.txt > nontalreads.txt
